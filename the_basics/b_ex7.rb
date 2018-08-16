44fec335153ec46ba2650a31bcee3b79585d55b7
Jorje Hernandez <thedeznon@hotmail.com>
2018-08-15 19:10:07 -0700

    Error explanation.

---
+++ the_basics/b_ex7.rb
@@ -0,0 +1,7 @@
+=begin
+The error below:
+  SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+  from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+Tells me that there is an incorrect closing bracket. Meaning the system expected a '}' instead of the ')'.
+=end
