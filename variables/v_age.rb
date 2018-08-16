ae99297534cd87b93dd9114ec2883147022bab5b
Jorje Hernandez <thedeznon@hotmail.com>
2018-08-16 16:51:22 -0700

    Age multiplier program.

---
+++ variables/v_age.rb
@@ -0,0 +1,11 @@
+puts "May I please have your age for verification purposes?"
+age = gets.chomp
+puts "Thank you."
+puts "In 10 years you will be:"
+puts age.to_i + 10
+puts "In 20 years you will be:"
+puts age.to_i + 20
+puts "In 30 years you will be:"
+puts age.to_i + 30
+puts "In 40 years you will be:"
+puts age.to_i + 40
