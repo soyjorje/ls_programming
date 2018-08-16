ed4de3393a6f9e82328bcf12c84923f5d64c075a
Jorje Hernandez <thedeznon@hotmail.com>
2018-08-16 16:53:16 -0700

    Code changes

--- variables/v_age.rb
+++ variables/v_age.rb
@@ -1,11 +1,11 @@
 puts "May I please have your age for verification purposes?"
-age = gets.chomp
+age = gets.chomp.to_i
 puts "Thank you."
 puts "In 10 years you will be:"
-puts age.to_i + 10
+puts age + 10
 puts "In 20 years you will be:"
-puts age.to_i + 20
+puts age + 20
 puts "In 30 years you will be:"
-puts age.to_i + 30
+puts age + 30
 puts "In 40 years you will be:"
-puts age.to_i + 40
+puts age + 40
