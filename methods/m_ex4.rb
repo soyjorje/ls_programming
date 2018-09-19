b56b6239add8254cd8d4b191787a3024f8d8493e
Jorje Hernandez <thedeznon@hotmail.com>
2018-09-18 21:33:23 -0700

    The method will return nothing

---
+++ methods/m_ex4.rb
@@ -0,0 +1,9 @@
+def scream(words)
+  words = words + "!!!!!"
+  return
+  puts words
+end
+
+scream("Yipeee")
+
+# This method will not return anything to the screen because of the exlpicit return call
