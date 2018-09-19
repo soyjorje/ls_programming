b6b5d104f9d78fea2a525bec30fba75a8987d786
Jorje Hernandez <thedeznon@hotmail.com>
2018-09-18 21:42:47 -0700

    The edited script now allows for the words to be printed to screen

---
+++ methods/m_ex5.rb
@@ -0,0 +1,8 @@
+def scream(words)
+  words = words + "!!!!!"
+  p words
+end
+ 
+scream("Yipeee")
+
+# This edit to the previous script will now allow the Words "Yipeee!!!!!" to be printed to the screen
