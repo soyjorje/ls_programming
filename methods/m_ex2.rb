c3fe7cc3d8264e0cf4b2c338cf61c5077a0bd9ba
Jorje Hernandez <thedeznon@hotmail.com>
2018-09-18 21:20:12 -0700

    The answers to what each value would be for each expression

---
+++ methods/m_ex2.rb
@@ -0,0 +1,10 @@
+1. x = 2
+# Value would be 2
+2. puts x = 2
+# Value would be Nil
+3. p name = "Joe"
+# Value would be "Joe"
+4. four = "four"
+# Value would be "four"
+5. print something = "nothing"
+# Value would be Nil
