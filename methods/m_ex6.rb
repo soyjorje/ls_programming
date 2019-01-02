1c4d3c5a998e304be1871d34f5e573fcef3f79e0
Jorje Hernandez <thedeznon@hotmail.com>
2019-01-01 23:30:32 -0700

    Exercise 6, defining why the error was given.

---
+++ methods/m_ex6.rb
@@ -0,0 +1,6 @@
+# The error below, indicates a missing value of two needed. 
+
+ArgumentError: wrong number of arguments (1 for 2)
+  from (irb):1:in `calculate_product'
+  from (irb):4
+  from /Users/username/.rvm/rubies/ruby-2.0.0-p353/bin/irb:12:in `<main>'
